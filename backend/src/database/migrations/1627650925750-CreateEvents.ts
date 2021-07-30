7b0fcd15feacfe47571987f5f26ec5ecd4dc56b9
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-07-30 10:52:00 -0300

    migrations for users and events

---
+++ backend/src/database/migrations/1627650925750-CreateEvents.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateEvents1627650925750 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"events",
+                columns:[
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                      },
+                      {
+                        name: "description",
+                        type: "varchar",
+                      },
+                      {
+                        name: "start_time",
+                        type: "varchar",
+                      },
+                      {
+                        name: "end_time",
+                        type: "varchar",
+                      },                      
+                      {
+                        name: "start_date",
+                        type: "varchar",
+                      },
+                      {
+                        name: "end_date",
+                        type: "varchar",
+                      },
+
+                ]
+
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("events");
+    }
+
+}
