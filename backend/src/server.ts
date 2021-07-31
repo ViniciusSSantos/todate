4b57dae4735095ab294bb4f1ef66e891dac34e6c
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-07-31 05:03:54 -0300

    routes and json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import "reflect-metadata";
 import express from "express";
+import { router } from "./routes"
 import "./database";
+require('dotenv').config()
+
 const app = express();
 
+app.use(express.json());
+app.use(router);
 app.listen(3333, () => console.log("Server is up and running"));
 
