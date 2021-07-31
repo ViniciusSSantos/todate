2e753a5f45e3257b61ca1108120e9c598f37626f
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-07-31 05:11:13 -0300

    List events created by user service

---
+++ backend/src/services/ListCreatedByEventsService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from "typeorm";
+import { EventsRepositories } from "../repositories/EventsRepositories";
+
+
+
+class ListCreatedByEventsService{
+    async execute(user_id: string){
+        const eventsRepository = getCustomRepository(EventsRepositories);
+
+        const eventsCreatedByUser = await eventsRepository.find({
+            where:{
+                created_by: user_id,
+            }
+        })
+
+        return eventsCreatedByUser
+
+    }
+}
+
+export { ListCreatedByEventsService }
