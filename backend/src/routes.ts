722832aa71595a0ef83e461c328f341425b3a679
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-07-31 07:56:53 -0300

    routes for editing and deleting events

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
+
 import { CreateEventController } from "./controllers/CreateEventController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ListCreatedByEventsController } from "./controllers/ListCreatedByEventsController";
+import { UpdateEventController } from "./controllers/UpdateEventController";
+import { DeleteEventController } from "./controllers/DeleteEventController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createEventController = new CreateEventController();
+const listCreatedByEventsController = new ListCreatedByEventsController()
+const updateEventController = new UpdateEventController()
+const deleteEventController = new DeleteEventController()
+
 
 router.post("/users", createUserController.handle),
 router.post("/events", createEventController.handle)
 
-router.get("/user/events", )
+router.get("/user/events",listCreatedByEventsController.handle)
+router.put("/user/events", updateEventController.handle)
+router.delete("/user/events", deleteEventController.handle )
 
 export{router}
