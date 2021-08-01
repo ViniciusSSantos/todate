10b7301e7ab102c127c45b944b282376c67b679a
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-07-31 21:37:06 -0300

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import { router } from "./routes";
+import cors from 'cors';
 import "./database";
 require('dotenv').config()
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(router);
