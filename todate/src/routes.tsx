017d7fbc630cb07cf8e72094151dde92dde6e182
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-08-02 16:23:56 -0300

    new routes

--- todate/src/routes.tsx
+++ todate/src/routes.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import events from './pages/events';
 
 import Landing from "./pages/landing"
+import Login from './pages/login';
 import SignUp from './pages/signup';
-
+import Events from './pages/events'
 
 
 function Routes(){
@@ -13,7 +15,9 @@ function Routes(){
         <BrowserRouter>
         <Switch>
         <Route path ="/landing" exact component = {Landing}/>
-        <Route path ="/signup"  component = {SignUp} />    
+        <Route path ="/signup"  component = {SignUp} /> 
+        <Route path ="/login"  component = {Login}  />   
+        <Route path = "/user/:id" component = {Events}  />
 
         </Switch>
         
