9e86f6bb8119c15cfb1b328a74d1cb9e5d3b8cdb
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-08-02 12:05:03 -0300

    new routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,8 +5,7 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { ListCreatedByEventsController } from "./controllers/ListCreatedByEventsController";
 import { UpdateEventController } from "./controllers/UpdateEventController";
 import { DeleteEventController } from "./controllers/DeleteEventController";
-import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-
+import { GetUserController } from "./controllers/GetUserController";
 const router = Router();
 
 const createUserController = new CreateUserController();
@@ -14,12 +13,11 @@ const createEventController = new CreateEventController();
 const listCreatedByEventsController = new ListCreatedByEventsController()
 const updateEventController = new UpdateEventController()
 const deleteEventController = new DeleteEventController()
-const authenticateUserController = new AuthenticateUserController()
-
+const getUserController = new GetUserController()
 
 router.post("/users", createUserController.handle),
 router.post("/events", createEventController.handle)
-router.post("/login", authenticateUserController.handle);
+router.post("/login", getUserController.handle);
 
 router.get("/user/:id/events",listCreatedByEventsController.handle)
 router.put("/user/:id/events", updateEventController.handle)
