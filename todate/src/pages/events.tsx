75f3dd98c1aa959eaef0f8cb9f799e3d9c2246cf
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-08-03 08:10:19 -0300

    modify events form

--- todate/src/pages/events.tsx
+++ todate/src/pages/events.tsx
@@ -1,8 +1,9 @@
-import { Console } from "console";
 import React, { FormEvent, useEffect, useState } from "react";
 import {useParams } from 'react-router-dom'
 import api from "../services/api";
 import "../styles/pages/events.css";
+import Collapsible from 'react-collapsible';
+
 
 interface event {
     id: string; 
@@ -23,7 +24,7 @@ interface userParams {
 export default function Events(){
     const params = useParams<userParams>();
     const [events, setEvents] = useState<event[]>([]);
-    const [id, setId] = useState(0)
+    const [id, setId] = useState('')
     const [created_by, setCreated_by] = useState(params.id);
     const [description, setDescription] = useState('');
     const [start_time, setStart_time] = useState('');
@@ -36,7 +37,7 @@ export default function Events(){
         api.get(`user/${params.id}`).then(response =>{
             setEvents(response.data)
         })
-    }, [])
+    }, [params.id])
 
 
     async function handleSubmit(event: FormEvent){
@@ -60,7 +61,7 @@ export default function Events(){
     }
 
     async function HandleUpdate(){
-        api.patch(`user/${id}`,{
+        api.put(`user/${params.id}`,{
             id: id,
             description: description,
             start_time: start_time,
@@ -83,14 +84,70 @@ export default function Events(){
                       <ol className="gradient-list" key = {event.id} >
                           
                           <h1> INFORMAÇÕES DO EVENTO</h1>
-                          <button onClick = {() => HandleDelete(event.id)}>aperte aqui para deletar</button>
-                          <li>{event.description} </li>
-                          <li>{event.start_date} </li>
-                          <li>{event.end_date} </li>
-                          <li>{event.start_time} </li>
-                          <li>{event.end_time} </li>
+                          <button className="confirm-button" onClick = {() => HandleDelete(event.id)}>aperte aqui para deletar</button>                         
+                          <li  >{event.description} </li>
+                          <li >{event.start_time} </li>
+                          <li  >{event.end_time} </li>
+                          <li >{event.start_date} </li>
+                          <li >{event.end_date} </li>                        
                       </ol>
-                    
+                      <Collapsible trigger="modifique aqui" onOpen = {() => setId(event.id)}>
+                      <form onSubmit = {HandleUpdate} className="create-user-form">
+                            <fieldset>
+                                <legend>modifique evento</legend>  
+                                    <div className="input-block">
+                                        <label htmlFor="name">start_time</label>
+                                        <input 
+                                        id="start_time"
+                                        value = {start_time} 
+                                        onChange = {event => setStart_time(event.target.value)}
+                                        />
+                                    </div>
+
+                                    <div className="input-block">
+                                        <label htmlFor="name">end_time</label>
+                                        <input 
+                                        id="end_time"
+                                        value = {end_time} 
+                                        onChange = {event => setEnd_time(event.target.value)}
+                                        />
+                                    </div>
+                                    <div className="input-block">
+                                        <label htmlFor="name">start_date</label>
+                                        <input 
+                                        id="start_date"
+                                        value = {start_date} 
+                                        onChange = {event => setStart_date(event.target.value)}
+                                        />
+                                    </div>
+                                    <div className="input-block">
+                                        <label htmlFor="name">end_date</label>
+                                        <input 
+                                        id="end_date"
+                                        value = {end_date} 
+                                        onChange = {event => setEnd_date(event.target.value)}
+                                        />
+                                    </div>
+
+                                                    
+                                    <div className="input-block">
+                                    <label htmlFor="about">descrição <span>Máximo de 300 caracteres</span></label>
+                                    <textarea id="description" 
+                                    maxLength={300} 
+                                    value = {description} 
+                                    onChange = {event => setDescription(event.target.value)}
+                                    />
+                                    </div>              
+                            </fieldset>
+
+                            <button className="confirm-button" type="submit">
+                            Confirmar
+                            </button>
+                        </form>
+
+                      </Collapsible>
+
+
                    
 
 
@@ -108,7 +165,7 @@ export default function Events(){
 
                         <form onSubmit = {handleSubmit} className="create-user-form">
                             <fieldset>
-                                <legend>login</legend>  
+                                <legend>Novo evento</legend>  
                                     <div className="input-block">
                                         <label htmlFor="name">start_time</label>
                                         <input 
