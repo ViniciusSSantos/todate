627ddb9f74167085ef58493002e7c67a90686087
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-08-25 08:13:01 -0300

    ensureAuthenticated

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,26 +1,40 @@
+// IMPORTS
 import { Router } from "express";
-
 import { CreateEventController } from "./controllers/CreateEventController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ListCreatedByEventsController } from "./controllers/ListCreatedByEventsController";
 import { UpdateEventController } from "./controllers/UpdateEventController";
 import { DeleteEventController } from "./controllers/DeleteEventController";
-import { GetUserController } from "./controllers/GetUserController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+
+
 const router = Router();
 
+//CONTROLLERS
 const createUserController = new CreateUserController();
 const createEventController = new CreateEventController();
 const listCreatedByEventsController = new ListCreatedByEventsController()
 const updateEventController = new UpdateEventController()
 const deleteEventController = new DeleteEventController()
-const getUserController = new GetUserController()
+const authenticateUserController = new AuthenticateUserController()
 
-router.post("/users", createUserController.handle),
-router.post("/events", createEventController.handle)
-router.post("/login", getUserController.handle);
 
-router.get("/user/:id",listCreatedByEventsController.handle)
-router.put("/user/:id", updateEventController.handle)
-router.delete("/user/:id", deleteEventController.handle)
+
+
+// POST ROUTES
+router.post("/createusers", createUserController.handle),
+router.post("/createevents", createEventController.handle)
+router.post("/login", authenticateUserController.handle);
+
+// GET EVENTS ROUTES 
+router.get("/user/events", ensureAuthenticated, listCreatedByEventsController.handle)
+
+// PUT EVENTS ROUTES
+router.put("/user/events", ensureAuthenticated, updateEventController.handle)
+
+// DELETE EVENTS ROUTES
+router.delete("/user/events",ensureAuthenticated, deleteEventController.handle)
+
 
 export{router}
