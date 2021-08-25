ce44a6dcc4e68ba91df2093de295e9771925726a
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-08-25 10:36:05 -0300

    created_by filled by middleware

--- backend/src/controllers/CreateEventController.ts
+++ backend/src/controllers/CreateEventController.ts
@@ -4,24 +4,26 @@ import { CreateEventService } from "../services/CreateEventService";
 
 class CreateEventController {
     async handle(request: Request, response: Response) {
-      const { created_by,
+      const {user_id} = request;
+
+      const {
         description,
         start_time,
         end_time,
         start_date,
         end_date, } = request.body;
-  
+        
       const createEventService = new CreateEventService();
   
       const event = await createEventService.execute({
-        created_by,
+        created_by: user_id,
         description,
         start_time,
         end_time,
         start_date,
         end_date,        
       });
-  
+
       return response.json(event);
     }
   }
