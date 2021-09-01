c6073137751fd133ad06554fe3d0c847de44526b
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-09-01 11:27:03 -0300

    shared module

--- todate-angular/src/app/login/login.module.ts
+++ todate-angular/src/app/login/login.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -12,7 +13,8 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class LoginModule { }
