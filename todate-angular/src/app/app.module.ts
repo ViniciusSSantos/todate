20278770631c4f622121322d7a12aaf0fbdcae35
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-09-01 08:42:56 -0300

    imports, providers and declarations updates

--- todate-angular/src/app/app.module.ts
+++ todate-angular/src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { SignupModule } from './signup/signup.module';
+import { LoginModule } from './login/login.module';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+
+
+import { AuthService } from './services/auth.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,    
+    HttpClientModule,
+    SignupModule,
+    LoginModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
