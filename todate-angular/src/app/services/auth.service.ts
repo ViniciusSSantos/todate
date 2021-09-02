60a5aed566d28ea062a0330f24cb2e034c77471f
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-09-02 18:21:57 -0300

    user authentication service

--- todate-angular/src/app/services/auth.service.ts
+++ todate-angular/src/app/services/auth.service.ts
@@ -1,19 +1,56 @@
 import { User } from './../models/User';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from "moment";
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  loginApi = 'localhost:3333/login';
 
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
   login(email:string, password:string){
-    return this.http.post<User>(this.loginApi, {email, password});            
+    return this.http.post('http://localhost:3333/login', {email, password})
+    .subscribe( res => {
+      this.setSession(res);
+      this.router.navigate(['/events'])
+    
+    })            
   }
+
+
+  private setSession(authResponse: object){
+    const expiresAt = moment().add(259200, 'second');
+    localStorage.setItem("token", JSON.stringify(authResponse));
+    localStorage.setItem("expiresAt", expiresAt.toString())
+    
+  }
+
+  logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiresAt");
+  }
+
+  public isLoggedIn() {
+      return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+      return !this.isLoggedIn();
+  }
+
+    
+  getExpiration() {
+    const expiration = localStorage.getItem("expiresAt")!;
+    const expiresAt = new Date(expiration);
+    return moment(expiresAt);
+}    
 
 }
