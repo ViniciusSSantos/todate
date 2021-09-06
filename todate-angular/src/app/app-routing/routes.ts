7bb06211bd8f0fb36fc5bbf1ec7a13b29786807b
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-09-06 14:16:34 -0300

    List events component

--- todate-angular/src/app/app-routing/routes.ts
+++ todate-angular/src/app/app-routing/routes.ts
@@ -1,8 +1,8 @@
 import { AuthGuard } from './../routesguards/auth.guard';
-import { EventsComponent } from './../events/events.component';
 import { SignupComponent } from './../signup/signup.component';
 import { LandingComponent } from './../landing/landing.component';
 import { LoginComponent } from './../login/login.component';
+import { ListEventsComponent } from '../events/list-events/list-events.component';
 import { Routes } from '@angular/router';
 
 
@@ -11,6 +11,6 @@ export const routes: Routes =[
     {path: 'login', component:LoginComponent},
     {path: 'landing', component: LandingComponent},
     {path:'signup', component: SignupComponent},
-    {path: 'events', component: EventsComponent, canActivate: [AuthGuard]},
+    {path: 'events', component: ListEventsComponent, canActivate: [AuthGuard]},
     { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ]
