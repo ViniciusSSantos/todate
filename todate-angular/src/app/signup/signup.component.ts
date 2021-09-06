cd0c48918dd92e97efa731eeb9fd2d1a95ad6f6f
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-09-06 13:57:41 -0300

    create users service

--- todate-angular/src/app/signup/signup.component.ts
+++ todate-angular/src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
+import { CreateUsersService } from './../services/create-users.service';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -10,25 +11,25 @@ export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(
+    private fb: FormBuilder,
+    private createUsersService: CreateUsersService) { 
     this.signupForm = this.fb.group({
       name: [null],
       email: [null],
       password: [null],
-      
     })
 
   
   }
 
-  ngOnInit(): void {
-
-    
-
-  }
+  ngOnInit(): void { }
 
   onSubmit(){
     console.log(this.signupForm.value);
+    const signupValues = this.signupForm.value;
+    this.createUsersService.Create(signupValues.name, signupValues.email, signupValues.password)
+        
   }
 
 
